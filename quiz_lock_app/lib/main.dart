df53b3ad39e6ce6fd53b93994c160f1314d4d1b0
SeungYeon04 <100332811+SeungYeon04@users.noreply.github.com>
2026-06-25 22:35:16 +0900

    add : 폰트 적용

--- quiz_lock_app/lib/main.dart
+++ quiz_lock_app/lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'quiz/screens/quiz_screen.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // Web에서 한글이 잠깐 깨져 보이는 현상 방지: 앱 시작 전 폰트 로드
-  await GoogleFonts.pendingFonts([GoogleFonts.notoSansKr()]);
+void main() {
   runApp(const MyApp());
 }
 
@@ -15,13 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final base = ThemeData(
-      colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF6C5CE7)),
-      useMaterial3: true,
-    );
     return MaterialApp(
       title: '퀴즈 앱',
-      theme: base.copyWith(textTheme: GoogleFonts.notoSansKrTextTheme(base.textTheme)),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF6C5CE7)),
+        useMaterial3: true,
+        fontFamily: 'Kkuk',
+      ),
       home: const QuizScreen(),
     );
   }
